fe76687d5601a3e2e9f5cbd204faaf2b9eb204da
Tharukshi Diyunugala <139604971+TharukshiDiyunugala@users.noreply.github.com>
2025-09-02 00:11:13 +0530

    Basic setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+import RootLayout from '../layout'
+
+const Rootlayout = ({children}:{children: ReactNode}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default RootLayout
